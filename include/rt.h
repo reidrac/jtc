9b55638780abfbb6591eadf6ac7d5aeca47cc6be
Juan J. Martinez <jjm@usebox.net>
2014-01-07 18:23:52 +0000

    don't remove the refernce if it is the object we're returning

--- include/rt.h
+++ include/rt.h
@@ -657,8 +657,8 @@ obj *o_return(st **ctx, obj *o) {
 
     HASH_ITER(hh, *ctx, s, tmp) {
         HASH_DEL(*ctx, s);
-		s->o->ref--;
 		if(s->o != o) {
+			s->o->ref--;
 			o_del(&s->o);
 		}
         free(s);
