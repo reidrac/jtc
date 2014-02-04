d39e5617f67518d05c547ab8898ce9890d63843f
Juan J. Martinez <jjm@usebox.net>
2014-02-04 19:15:46 +0000

    update C output example

--- examples/fib-0.6.c
+++ examples/fib-0.6.c
@@ -1,14 +1,14 @@
 /*
- * jtc ver 0.3
- * source: fib.jt
+ * jtc ver 0.6.1
+ * source: examples/fib.jt
  */
 #include "rt.h"
 
-obj *_fib11 (obj * n);
-obj *_main15 ();
+obj *_fib_1 (obj * n);
+obj *_main_1 ();
 
 obj *
-_fib11 (obj * n)
+_fib_1 (obj * n)
 {
   st *_ctx = NULL;
   store (&_ctx, 4, 2, n);
@@ -16,31 +16,34 @@ _fib11 (obj * n)
     {
       return o_return (&_ctx, retrieve (&_ctx, 6, 2));
     }
+
   return o_return (&_ctx,
 		   o_op (8, ADD,
-			 _fib11 (o_op
+			 _fib_1 (o_op
 				 (8, SUB, retrieve (&_ctx, 8, 2),
-				  o_int (8, 1))), _fib11 (o_op (8, SUB,
+				  o_int (8, 1))), _fib_1 (o_op (8, SUB,
 								retrieve
 								(&_ctx, 8, 2),
 								o_int (8,
 								       2)))));
+
   return o_return (&_ctx, o_int (0, 0));
 }
 
 obj *
-_main15 ()
+_main_1 ()
 {
   st *_ctx = NULL;
-  println (1, _fib11 (o_int (11, 10)));
+  println (1, _fib_1 (o_int (11, 10)));
   return o_return (&_ctx, o_int (12, 0));
+
   return o_return (&_ctx, o_int (0, 0));
 }
 
 int
 _ep ()
 {
-  obj *o = _main15 ();
+  obj *o = _main_1 ();
   return o_lval (0, o);
 }
 
